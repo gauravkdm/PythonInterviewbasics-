d151388ec6a5d59991d69f3c2f04e5b7e220288d
Gaurav Kadam <gnksip@gmail.com>
2021-08-19 11:25:36 +0530

    Compare two list using Sort Method :

---
+++ test12.py
@@ -0,0 +1,19 @@
+list1= [1,2,3,4,5,6]
+list2= [6,5,4,3,2,1]
+list3= [9,3,4,5,6,1]
+
+list1.sort()
+list2.sort()
+list3.sort()
+if list1 == list2 :
+    print("Yes they are same ")
+else :
+    print("No they are not same ")
+if list1 == list3:
+    print("Yes they are same ")
+else :
+    print("No they are not same ")
+    
+    # Output :
+    # Yes they are same 
+    # No they are not same  
