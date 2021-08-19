53a0707dec84f083a9b8236ecd7c7e90e3b4ea52
Gaurav Kadam <gnksip@gmail.com>
2021-08-19 10:55:31 +0530

    Convert List into String Using Forloop :

---
+++ test5.py
@@ -0,0 +1,10 @@
+list1=["hello",'my','name','is','gaurav','narayan','kadam']
+def convertintolist(list1):
+    newstr=''
+    for i in list1:
+        newstr = newstr + i
+    return newstr
+print(convertintolist(list1))
+
+# Output :
+# hellomynameisgauravnarayankadam
