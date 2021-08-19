79da1b31d0891758c01f2566381af90586e61112
Gaurav Kadam <gnksip@gmail.com>
2021-08-19 11:00:43 +0530

    Convert List into String Using Map Method :

---
+++ test7.py
@@ -0,0 +1,6 @@
+list1=["hello",'my','name','is','gaurav','narayan','kadam']
+convertList = ' '.join(map(str,list1)) # using map function 
+print (convertList)
+
+# Output :
+# hello my name is gaurav narayan kadam
