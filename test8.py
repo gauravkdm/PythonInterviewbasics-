3b3442ca081904b9c4b004ed7636ba5282d95c71
Gaurav Kadam <gnksip@gmail.com>
2021-08-19 11:03:49 +0530

    Add Element in List Using Append Function

---
+++ test8.py
@@ -0,0 +1,10 @@
+list1=['Hello','my','name','is ','gaurav']
+print("Actual list :",list1)
+suggestname = input("Enter the name :")
+list1.append(suggestname)
+print("Updated list :",list1)
+
+# Output :
+# Actual list : ['Hello', 'my', 'name', 'is ', 'gaurav']
+# Enter the name :kadam
+# Updated list : ['Hello', 'my', 'name', 'is ', 'gaurav', 'kadam']
