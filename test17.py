7abd57e79f6f693e2e2c49b7df0395c92c8ae495
Gaurav Kadam <gnksip@gmail.com>
2021-08-20 20:32:26 +0530

    Creating Forloop in Python

---
+++ test17.py
@@ -0,0 +1,7 @@
+# How to create forloop in python ?
+list1= [10,20,30,40,50,60,70,80,90,100]
+for i in range (len(list1)):
+    print(list1[i],end=" ")
+
+# Output :
+# 10 20 30 40 50 60 70 80 90 100
