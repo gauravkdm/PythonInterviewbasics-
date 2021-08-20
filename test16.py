d0321e590a9b8c39351a5bd5c457119ea52609c3
Gaurav Kadam <gnksip@gmail.com>
2021-08-20 20:21:42 +0530

    Read txt file in Python

---
+++ test16.py
@@ -0,0 +1,8 @@
+# How to read txt file in Python 
+# For this program i had created a file called student.txt as an example 
+fileopentxt = open(r'D:\PythonInterviewbasics-\student.txt')
+readfile = fileopentxt.read()
+print(readfile)
+
+# Output :
+# Hello I am Gaurav Kadam . I am learning Python Currently but stil a begineer .
