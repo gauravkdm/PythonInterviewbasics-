5e6966791781f8d42506196ef68037504a089ca5
Gaurav Kadam <gnksip@gmail.com>
2021-08-24 16:48:19 +0530

    Simple Pyramid Using For loop

---
+++ test22.py
@@ -0,0 +1,15 @@
+# Simple Pyramid :
+
+n= int(input("Enter the number of rows :"))
+
+for i in range (0,n):
+    for j in range(0, i+1):
+        print("*" ,end="")
+    print()
+
+# Output :
+# *
+# **
+# ***
+# ****
+# *****
