297aa0bbd6ce3d3f29530cfa55a648b569ce307e
Gaurav Kadam <gnksip@gmail.com>
2021-08-25 19:03:08 +0530

    Representation of Bar Graph Using Matplotlib

---
+++ test38.py
@@ -0,0 +1,9 @@
+from matplotlib import pyplot as plt 
+Names= ['James','Rock','Dwayne','Henry']
+marks=[30,40,20,45]
+plt.bar(Names,marks,color='red')
+plt.title("Result")
+plt.xlabel("Names")
+plt.ylabel('Marks')
+plt.show()
+
