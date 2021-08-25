0db81b66d20696dfacbad707bb397c442c1fc4a7
Gaurav Kadam <gnksip@gmail.com>
2021-08-25 10:43:59 +0530

    Number Pattern using Forloop :

---
+++ test29.py
@@ -0,0 +1,15 @@
+# Display Number Pattern :
+
+n=int(input("Enter the number of rows :"))
+
+for i in range(1,n+1):
+    for j in range(i):
+        print(i,end=" ")
+    print()
+
+    # Output :
+# 1 
+# 2 2 
+# 3 3 3 
+# 4 4 4 4 
+# 5 5 5 5 5
