b20db7986b81fdc5d8f8c705f669c1ad324f9169
Gaurav Kadam <gnksip@gmail.com>
2021-08-31 13:52:32 +0530

    Print 'L' using Forloop :

---
+++ test56.py
@@ -0,0 +1,16 @@
+for row in range(7):
+    for col in range(5):
+        if (row==6 or col==0):
+            print("*",end=" ")
+        else:
+            print(end=" ")
+    print()
+
+# Output :
+# *     
+# *     
+# *
+# *
+# *
+# *
+# * * * * *
