b4ec70a95b9dc0fcdc59996dff4b153334f7df18
Gaurav Kadam <gnksip@gmail.com>
2021-08-31 13:25:54 +0530

    Print 'J' using Forloop :

---
+++ test54.py
@@ -0,0 +1,16 @@
+for row in range(7):
+    for col in range(5):
+        if ( row==0 or (row==6 and col>=0 and col<3) or col==2):
+            print("*",end="")
+        else:
+            print(end=" ")
+    print()
+
+# Output :
+# *****
+#   *  
+#   *
+#   *
+#   *
+#   *
+# ***
