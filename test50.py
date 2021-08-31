0e24fe78deebc25d4dcfe1fd4899f401b1de120f
Gaurav Kadam <gnksip@gmail.com>
2021-08-31 12:38:28 +0530

    Print 'F' using ForLoop :

---
+++ test50.py
@@ -0,0 +1,15 @@
+for row in range(7):
+    for col in range( 5):
+        if (row==0 or row ==3) or (col==0):
+            print("*",end=" ")
+        else:
+            print(end=" ")
+    print()
+# Output:
+# * * * * * 
+# *     
+# *
+# * * * * *
+# *
+# *
+# *
