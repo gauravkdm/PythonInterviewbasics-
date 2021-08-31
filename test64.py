a36451691714623a43e4e8d5b1a6adc4463f4cf4
Gaurav Kadam <gnksip@gmail.com>
2021-08-31 18:38:30 +0530

    Print 'T' using Forloop

---
+++ test64.py
@@ -0,0 +1,16 @@
+for row in range(7):
+    for col in range(5):
+        if (row==0 or(col==2 and row!=0) ):
+            print("*",end="")
+        else:
+            print(end=" ")
+    print()
+
+# Output :
+# *****
+#   *  
+#   *
+#   *
+#   *
+#   *
+#   *
