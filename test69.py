aaafbd22531de795d398c6d5e2263362e4a72341
Gaurav Kadam <gnksip@gmail.com>
2021-09-06 21:12:06 +0530

    Swapping two variables without using another variable :

--- test69.py
+++ test69.py
@@ -1,11 +1,27 @@
 #  Swapping two variables without third variable :
+
+# 1. Using Swapping Method :
 x=10
 y=20
 
-print("X value before swapping is :",x)
-print("Y value befor swapping is :",y)
+# print("X value before swapping is :",x)
+# print("Y value befor swapping is :",y)
 
-x,y = y,x
+# x,y = y,x
 
-print("X value after swapping is :",x)
-print('Y value after swapping is :',y)
+# print("X value after swapping is :",x)
+# print('Y value after swapping is :',y)
+
+
+# 2. Using Addition Method :
+
+
+x=x+y
+y=x-y
+x=x-y
+print("X value is ",x)
+print('Y value is ',y)
+
+
+
+
