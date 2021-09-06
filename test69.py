a3676b78055e2fc36ad47c390126b6c5fb494c5a
Gaurav Kadam <gnksip@gmail.com>
2021-09-06 20:37:55 +0530

    Swapping two variables without using third variable

---
+++ test69.py
@@ -0,0 +1,11 @@
+#  Swapping two variables without third variable :
+x=10
+y=20
+
+print("X value before swapping is :",x)
+print("Y value befor swapping is :",y)
+
+x,y = y,x
+
+print("X value after swapping is :",x)
+print('Y value after swapping is :',y)
