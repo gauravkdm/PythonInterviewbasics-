622e37213ccc4f639fff2bc39676c3b9840d6014
Gaurav Kadam <gnksip@gmail.com>
2021-09-06 20:21:26 +0530

    Print 1 to 100 wihtout using Numbers

---
+++ test68.py
@@ -0,0 +1,17 @@
+tempF=ord('F')
+print(tempF)
+tempd=ord('d')
+print(tempd)
+
+divide= tempF / tempF
+print(int(divide))
+divideint=int(divide)
+
+for i in range(divideint,tempd):
+    print(i)
+
+while(divideint<=tempd):
+    print(divideint)
+    divideint= divideint + 1
+
+
